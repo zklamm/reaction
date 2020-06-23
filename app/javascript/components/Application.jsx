c95e438bcf297cf6d9a856e10a99506e8fef1153
mo mac <trrl.mo@hotmail.com>
2020-06-23 12:53:55 -0600

    Update Application.jsx to contain a "/boards/:id" route; Added skeleton for Board

--- app/javascript/components/Application.jsx
+++ app/javascript/components/Application.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route } from "react-router-dom";
 import TopNav from "./shared/TopNav";
 import BoardsDashboardContainer from "./dashboard/BoardsDashboardContainer";
+import BoardContainer from "./board/BoardContainer";
 
 const Application = () => {
   return (
     <div>
       <TopNav />
       <Route path="/" exact component={BoardsDashboardContainer} />
+      <Route path="/boards/:id" exact component={BoardContainer} />
     </div>
   );
 };
