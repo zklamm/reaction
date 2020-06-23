a6045d56a7163cb7bbae5e79bd83b406182d3951
mo mac <trrl.mo@hotmail.com>
2020-06-23 12:13:07 -0600

    Seed Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,15 @@
 
 # Board.destroy_all
 
-# board1 = Board.create({title: "First board"})
+board1 = Board.create({title: "First board"})
 # board2 = Board.create({title: "Second board"})
+
+list1 = Board.lists.create(title: 'First list')
+list2 = Board.lists.create(title: 'Second list')
+
+list1_card1 = list1.cards.create(title: 'First card on first list', description: "Card 1", labels: ['blue'], due_date: '01-04-2021')
+list1_card2 = list1.cards.create(title: 'Second card on first list', description: "Card 2", labels: ['green'], due_date: '10-10-2020')
+
+list2_card1 = list2.cards.create(title: 'First card on second list', description: "Card 1", labels: ['red'], due_date: '01-12-2020')
+list2_card2 = list2.cards.create(title: 'Second card on second list', description: "Card 2", labels: ['grey'], due_date: '10-11-2020')
+list2_card3 = list2.cards.create(title: 'Third card on second list', description: "Card 3", labels: ['yellow'], due_date: '1-12-2020')
