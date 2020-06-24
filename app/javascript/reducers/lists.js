6a04a73a7371d24215bdea92d209a9581e1a03ff
John Isom <John.Isom2001@gmail.com>
2020-06-24 12:40:18 -0600

    Implement lists reducer for “FETCH_BOARD_SUCCESS”

--- app/javascript/reducers/lists.js
+++ app/javascript/reducers/lists.js
@@ -1,7 +1,23 @@
+const filterOutOldLists = (state, newLists) => {
+  // TODO: make this more efficient by using hash table
+  return state.filter((oldList) => {
+    return !newLists.some((newList) => newList.id == oldList.id);
+  });
+};
+
+const removeCardsFromLists = (lists) => {
+  return lists.map(({ cards, ...otherProps }) => ({ ...otherProps }));
+};
+
 export default function lists(state = [], action) {
   switch (action.type) {
     case "FETCH_BOARD_SUCCESS":
-      return state; // Grab new lists from action.payload.board.lists (no cards allowed)
+      const listsWithoutCards = removeCardsFromLists(
+        action.payload.board.lists
+      );
+      return filterOutOldLists(state, listsWithoutCards).concat(
+        listsWithoutCards
+      );
     default:
       return state;
   }
