62b242f401bd38cea0b0ec87afb5e67c56bbb586
John Isom <John.Isom2001@gmail.com>
2020-06-29 12:19:44 -0600

    Change card tiles from rendering static HTML to actual content

--- app/javascript/components/card/Card.jsx
+++ app/javascript/components/card/Card.jsx
@@ -1,30 +1,37 @@
 import React from "react";
+import { MONTHS } from "../../constants/Months";
 
-const Card = () => {
+const formatDate = (date_str) => {
+  const date = new Date(date_str);
+  return `${MONTHS[date.getMonth()]} ${date.getDate()}`;
+};
+
+const Card = ({ labels, cardId, title, dueDate, isDescription }) => {
   return (
     <div className="card-background">
       <div className="card ">
         <i className="edit-toggle edit-icon sm-icon"></i>
         <div className="card-info">
-          <div className="card-label green colorblindable"></div>
-          <div className="card-label yellow colorblindable"></div>
-          <div className="card-label red colorblindable"></div>
-          <div className="card-label orange colorblindable"></div>
-          <div className="card-label blue colorblindable"></div>
-          <div className="card-label purple colorblindable"></div>
-          <p>
-            Cards do many cool things. Click on this card to open it and learn
-            more...
-          </p>
+          {labels.map((label) => (
+            <div className={`card-label ${label} colorblindable`}></div>
+          ))}
+          <p>{title}</p>
         </div>
         <div className="card-icons">
-          <i className="clock-icon sm-icon overdue-recent completed">Aug 4</i>
-          <i className="description-icon sm-icon"></i>
-          <i className="comment-icon sm-icon"></i>
+          <i className="clock-icon sm-icon overdue-recent completed">
+            {formatDate(dueDate)}
+          </i>
+          {isDescription ? <i className="description-icon sm-icon"></i> : null}
         </div>
       </div>
     </div>
   );
 };
 
+// <div className="card-label green colorblindable"></div>
+// <div className="card-label yellow colorblindable"></div>
+// <div className="card-label red colorblindable"></div>
+// <div className="card-label orange colorblindable"></div>
+// <div className="card-label blue colorblindable"></div>
+// <div className="card-label purple colorblindable"></div>
 export default Card;
--- app/javascript/components/card/ExistingCards.jsx
+++ app/javascript/components/card/ExistingCards.jsx
@@ -4,8 +4,15 @@ import Card from "./Card";
 const ExistingCards = ({ cards }) => {
   return (
     <div id="cards-container" data-id="list-1-cards">
-      {cards.map((card, idx) => (
-        <Card {...card} key={idx} />
+      {cards.map((card) => (
+        <Card
+          labels={card.labels}
+          cardId={card.id}
+          title={card.title}
+          dueDate={card.due_date}
+          isDescription={!!card.description}
+          key={card.id}
+        />
       ))}
     </div>
   );
