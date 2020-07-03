3ed827bd88f27a61500917821380099cfa3c4264
Zac Klammer <zklamm@gmail.com>
2020-07-03 13:17:37 -0500

    Cleanup debugging statements and extraneous comments

--- app/javascript/components/board/BoardContainer.jsx
+++ app/javascript/components/board/BoardContainer.jsx
@@ -9,12 +9,9 @@ const mapStateToProps = (state, ownProps) => {
   if (ownProps.match.url.startsWith("/boards")) {
     boardId = +ownProps.match.params.id;
   } else {
-    console.log("here");
-    console.log(state.cards);
     card = state.cards.find((card) => {
       return card.id === +ownProps.match.params.id;
     });
-    console.log(card);
     if (card) {
       boardId = card.board_id;
     } else {
@@ -33,16 +30,6 @@ const mapStateToProps = (state, ownProps) => {
       board: null,
     };
   }
-  //   board: state.boards.find((board) => {
-  //     return board.id === +ownProps.match.params.id;
-  //   }),
-  // Check if the URL matches /boards, if so boardId will be +ownProps.match.params.id
-  // else check whether there is a card in the state based on +ownProps...
-  //   and then if there is a card, boardId will be card.board_id
-  //   else if there isn't a card yet, boardId will be null
-  //      if not a null
-  //        return an object, 2 props boardId: boardId, board: logic from line 18
-  //      else pass boardId: null
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
@@ -54,25 +41,3 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Board);
-
-// export default BoardContainer;
-
-/*
-
-Hierarchy
-=========
-- Application
-  - BoardsDashboardContainer (Route)
-  - BoardContainer (Route)
-    - Board
-      - Header
-      - ListContainer
-        -ExistingListsContainer
-          -ExistingLists
-            - List
-              - CardContainer
-                - Card
-                - NewCard
-        - NewList
-      - Sidebar
-*/
